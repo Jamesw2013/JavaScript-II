683d0908c1fbe39cd7d7563d754a33ec1823c2c7
JamesW <westjames2013@gmail.com>
2019-03-31 20:22:13 -0700

    Task 4 completed. MVP attained.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -65,8 +65,8 @@ function contains(item, list, cb) {
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
+// function removeDuplicates(array, cb) {
+//   // removeDuplicates removes all duplicate values from the given array.
+//   // Pass the duplicate free array to the callback function.
+//   // Do not mutate the original array.
+// }
